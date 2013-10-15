d26a7d5da50125f933232a12f0c570671e4837f0
teabait <hello.teaho@gmail.com>
2013-10-14 21:36:23 -0400

    Gemfile and finished app without awesome styling

--- server.rb
+++ server.rb
@@ -64,20 +64,40 @@ end
 # Form to create new songs
 
 get "/shows/:id/songs/new" do
+  erb :'songs/new'
 end
 
 # Create action - new songs for a show - redirects
 # to that song
 
 post "/shows/:id/songs" do
+  # get just the embed_url
+  @new_embed = params[:embed_url].slice! "http://www.youtube.com/watch?v=" 
+  
+  # create instance
+  song = Song.new(title: params[:title], embed_url: new_embed)
+
+  # get show to save song to show
+  show = Show.find_by(id: params[:id])
+  show.songs << song
+  # save instance
+  song.save
+
+  # redirect to show page
+  redirect "/#{show.id}"
 end
 
 # Lists all songs from the show
 
 get "/shows/:id/songs" do
+  @show = Show.find_by(id: params[:id])
+  @songs_array = @show.songs
+  erb :'songs/index'
 end
 
 # Shows just one song from the show
 
 get "/shows/:show_id/songs/:song_id" do
+  @song = Song.find_by(id: params[:song_id])
+  erb :'songs/show'
 end
